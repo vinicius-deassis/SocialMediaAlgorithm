565b685e57ac66a29b8d32996ff1bfd756641bb3
vinicius-deassis <vassisprof@gmail.com>
2023-04-11 15:10:40 -0300

    #1

---
+++ src/socialmediaalgorithm/Admin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package socialmediaalgorithm;
+
+/**
+ *
+ * @author Vinicius Assis
+ */
+public class Admin { 
+    public static void main(String[] args) {
+        Register conta1 = new Register("Vinicius", "Assis");
+        Register conta2 = new Register("Nicolas","Fernandes");
+        Register contateste = new Register("Vinicius","Assis");
+        
+        LoginControl cadastrando = new LoginControl();
+        cadastrando.Registering(conta1);
+        cadastrando.Registering(conta2);
+        
+        //System.out.println(cadastrando.ShowRegister());
+        
+        cadastrando.ChechRegister(contateste);
+    }
+    
+}
---
+++ src/socialmediaalgorithm/LoginControl.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socialmediaalgorithm;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author VInicius Assis
+ */
+public class LoginControl {
+    ArrayList<Register> users;
+    
+    public LoginControl(){
+        users = new ArrayList<>();    
+    }
+    
+    public void Registering(Register r){
+        users.add(r);
+    }
+   
+    public String ShowRegister(){
+        
+        String register = "";
+        for (Register i: users){
+            register += i;
+        }
+        return register;       
+    }
+    
+    public void ChechRegister(Register r){
+        int found = 0;
+        for(Register check:users){
+            if(check.getUser().equals(r.getUser()) && check.getPass().equals(r.getPass())){
+                System.out.println("ok");
+            }
+            else{
+                found = found +1;               
+            }            
+        if(found == users.size()){
+            System.out.println("Nao encontrado");
+            }
+        }
+    }
+}
+
