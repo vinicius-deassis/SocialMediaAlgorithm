c19f0c72d807e3746e2f98a375d76a542586a221
Nicolas <110586516+Nic-Listener@users.noreply.github.com>
2023-04-14 00:11:46 -0300

    Update PerfilUserDAO.java

--- src/dados/PerfilUserDAO.java
+++ src/dados/PerfilUserDAO.java
@@ -1,9 +1,6 @@
 package dados;
 
-import GUI.IPerfilUserDAO;
-
-import java.io.*;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 import java.io.*;
@@ -13,33 +10,14 @@ import java.util.Map;
 public abstract class PerfilUserDAO {
 
     private static final String FILE_PATH = "perfis.dat";
-
+    private ArrayList<Map<String, Integer>> usuarios = new ArrayList<>();
 
     public void salvar(Map<String, Integer> afinidades) {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(FILE_PATH);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(afinidades);
-            objectOut.close();
-            fileOut.close();
-        } catch (IOException ex) {
-            System.out.println("Erro ao salvar perfil do usuário: " + ex.getMessage());
-        }
+        usuarios.add(afinidades);
     }
 
-    public Map<String, Integer> getAfinidades() {
-        Map<String, Integer> afinidades = new HashMap<>();
-        try {
-            FileInputStream fileIn = new FileInputStream(FILE_PATH);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            afinidades = (Map<String, Integer>) objectIn.readObject();
-            objectIn.close();
-            fileIn.close();
-        } catch (IOException | ClassNotFoundException ex) {
-            System.out.println("Erro ao recuperar afinidades do usuário: " + ex.getMessage());
-        }
-        return afinidades;
+    public ArrayList<Map<String, Integer>> getUsuarios() {
+        return usuarios;
     }
 
 }
-
